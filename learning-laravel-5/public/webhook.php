908145d70eae035de5730aa6efe2a0f210af6eb3
Dominique Hostettler <dominiquehostettler@gmail.com>
2015-01-30 05:33:41 -0500

    webhook update

--- learning-laravel-5/public/webhook.php
+++ learning-laravel-5/public/webhook.php
@@ -21,7 +21,7 @@ $LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
 $REMOTE_REPO        = "git@github.com:hostdos/lfive.git";
 $BRANCH             = "master";
 
-if ( $_POST['payload'] ) {
+if (!$_POST['payload'] ) {
   // Only respond to POST requests from Github
 
   if( file_exists($LOCAL_REPO) ) {
